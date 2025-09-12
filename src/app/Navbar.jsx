5175b86ed75139f0d3a6195d6bff2d7dc141a524
milad998 <myaralbwd20@gmail.com>
2025-09-12 19:30:37 +0300

    Update Navbar.jsx

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -4,14 +4,15 @@ import logo from '../../public/logo.png'
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-body-tertiary d-flex justify-content-between align-items-center px-3 text-dark" style={{backgroundColor: '#ffffff00'}}>
-  <div className="container-fluid">
-    <Image src={logo} width={100} alt='logo'/>
-    <div>
-        <p>france</p>
-    </div>
-  </div>
-</nav>
+    <nav className="navbar bg-white shadow-sm px-4 py-2">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        {/* Logo */}
+        <Image src={logo} width={120} height={60} alt="logo" priority />
+
+        {/* Text */}
+        <p className="mb-0 fw-bold text-dark text-capitalize">France</p>
+      </div>
+    </nav>
   )
 }
 
