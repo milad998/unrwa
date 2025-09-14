6c67e62a9803abc708ab75e43c92a11a33077c83
milad998 <myaralbwd20@gmail.com>
2025-09-14 10:42:43 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import axios from "axios";
+
 
 export default function Home() {
   const router = useRouter();
